ab1f12d22660dd9ab088a0cd78f758fb24cc61e2
Joshua Thames <joshua.thames@lmco.com>
2020-08-14 09:09:04 -0400

    fixed script

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -8,9 +8,9 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
-wget https://github.com/ChrisTitusTech/zsh/raw/master/.zshrc -O ~/.zshrc
 mkdir -p "$HOME/.zsh"
-wget https://github.com/ChrisTitusTech/zsh/raw/master/.zsh/aliasrc -O ~/.zsh/aliasrc
+cp .zshrc "$HOME/.zsh/."
+cp aliasrc "$HOME/.zsh/."
 
 # plugins
 git clone --depth=1  https://github.com/zsh-users/zsh-autosuggestions.git "$HOME/.zsh/zsh-autosuggestions"
@@ -18,4 +18,4 @@ git clone --depth=1  https://github.com/zsh-users/zsh-syntax-highlighting.git "$
 git clone --depth=1  https://github.com/wting/autojump.git "$HOME/.zsh/autojump"
 git clone --depth=1  https://github.com/romkatv/powerlevel10k.git "$HOME/.zsh/powerlevel10k"
 
-touch "$HOME/.cache/zshhistory
+touch "$HOME/.cache/zshhistory"
